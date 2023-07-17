14beb4168971a79062c5c285ce968916995a8821
MiroslavJD <miroslavjd1@gmail.com>
2023-07-17 06:20:09 +0300

    Initial commit

---
+++ ProcessGo/ProcessGo1.UsingDLL/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessGo1.UsingDLL
+{
+    public class Class1
+    {
+        public void UnsortedText()
+        {
+            string path = "C:\\Users\\Prince\\Desktop\\ProcessGo\\ProcessGo\\ProcessGo\\bin\\Debug\\UnsortedFile.txt";
+            Process.Start(path);
+        }
+        public void SortedText()
+        {
+            string path2 = "C:\\Users\\Prince\\Desktop\\ProcessGo\\ProcessGo\\ProcessGo\\bin\\Debug\\SortedFile.txt";
+            Process.Start(path2);
+        }
+    }
+}
